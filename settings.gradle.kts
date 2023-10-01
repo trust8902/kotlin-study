8e5c1562b0099abda67f093aabb1ab4c4acdbbfd
jaden <chaewh@firststory.co.kr>
2023-10-01 21:49:47 +0900

    [KS01-001] 초도커밋

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "kottlin-study"
---
+++ src/test/kotlin/kr/co/firststory/kottlinstudy/KottlinStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.firststory.kottlinstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KottlinStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
